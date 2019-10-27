720c9cd22b57600782add75146b4d66d9d87e4e8
Lhendalie <madlen_koleva9@abv.bg>
2019-10-27 14:30:20 +0000

    Added player movement

---
+++ Assets/Scripts/Player_Animation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Player_Animation : NetworkBehaviour
+{
+    public Animator playerAnimator;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        CheckForPlayerInput();
+    }
+
+    void CheckForPlayerInput()
+    {
+        if (!isLocalPlayer)  //  only for current player
+        {
+            return;
+        }
+
+        if(Mathf.Abs(Input.GetAxis("Vertical")) > 0 ||
+            Mathf.Abs(Input.GetAxis("Horizontal")) > 0)  // Get the user input and plays the animation for it
+        {
+            playerAnimator.SetBool("Moving", true);
+        }
+
+        else
+        {
+            playerAnimator.SetBool("Moving", false);  // Stop Animation
+        }
+    }
+}
---
+++ Assets/Scripts/Player_Network.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Player_Network : NetworkBehaviour
+{
+    public GameObject firstPersonCharacter;
+    public GameObject[] characterModel;
+
+    public override void OnStartLocalPlayer() // This will run for the specific online character
+    {
+        GetComponent<FirstPersonController>().enabled = true;
+        firstPersonCharacter.SetActive(true);
+
+        foreach(GameObject go in characterModel)
+        {
+            go.SetActive(false); // Disable ethan model
+        }
+    }
+
+}
+
